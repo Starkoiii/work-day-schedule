4cb25078426f51e7a71a75401f19227a66a2e92f
Starkoiii <131171379+Starkoiii@users.noreply.github.com>
2023-07-11 16:05:21 -0500

    fixed bugs in html for time changes

--- assets/script.js
+++ assets/script.js
@@ -7,42 +7,20 @@ $(function () {
 // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? 
+  // var textMe =  $("textarea")
+  // localStorage.setItem("text", textMe.val(""));
   $('.saveBtn').on("click", function() {
-    // var textMe =  $("textarea")
-    // localStorage.setItem("text", textMe.val(""));
     var timeKey = $(this).parent().attr('id').split('-')[1];
     var valTxt = $(this).parent().find('.description').val();
     localStorage.setItem(timeKey, valTxt);
   })
-
   });
-  // How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  
-  
+
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? 
-  var currentHour = dayjs().format('H');
-
-  $(".time-block").each(function() {
-  var blockHour = $(this).attr('id').split('-')[1];
-
-    if (blockHour < currentHour) {
-      $(this).find('.description').addClass('past');
-    } else if (blockHour === currentHour) {
-      $(this).find('.description').addClass('present');
-    } else {
-      $(this).find('.description').addClass('future');
-    };
-  });
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
-var currentHour = dayjs().format('H');
+var currentHour = dayjs().format('HH');
   $(".time-block").each(function() {
   var blockHour = $(this).attr('id').split('-')[1];
 
@@ -57,9 +35,8 @@ var currentHour = dayjs().format('H');
     } else {
       $(this).find('.description').addClass('future');
     };
-
+})
 
 // TODO: Add code to display the current date in the header of the page.
 var today = dayjs();
-  $('#currentDay').text(today.format('dddd, MMMM D h:mm a'));
-});
+$('#currentDay').text(today.format('dddd, MMMM D h:mm a'));
